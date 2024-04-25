8b3564d89debf5b739e8419128e220852bdb9e85
TonAttapongJ <ton.attapong.j@gmail.com>
2024-04-25 16:01:17 +0700

    update: baseUrl constant

--- e-market/Core/Constant/CoreConstants.swift
+++ e-market/Core/Constant/CoreConstants.swift
@@ -8,5 +8,7 @@
 import Foundation
 
 struct CoreConstants {
-  static let baseUrl: URL = URL(string: "https://615dedf1-36b8-40fd-8fc4-fbc0998133cb.mock.pstmn.io")!
+  // https://615dedf1-36b8-40fd-8fc4-fbc0998133cb.mock.pstmn.io
+  // https://c8d92d0a-6233-4ef7-a229-5a91deb91ea1.mock.pstmn.io
+  static let baseUrl: URL = URL(string: "https://c8d92d0a-6233-4ef7-a229-5a91deb91ea1.mock.pstmn.io")!
 }
